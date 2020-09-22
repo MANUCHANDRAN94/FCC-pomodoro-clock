62ce1743e983853775d659efdadd5e5ac5c80a59
MANUCHANDRAN94 <m4manu001@gmail.com>
2020-09-22 15:31:04 +0530

    Updates

---
+++ precache-manifest.67d67e8f9b20acaf873491c1d51e7a14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02a92dbb621559a9d25ff9b18395f4e",
+    "url": "/FCC-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "6cdc2a3ecebc3f577837",
+    "url": "/FCC-pomodoro-clock/static/css/main.3836cdd9.chunk.css"
+  },
+  {
+    "revision": "1dc8409f770c1c9df0cd",
+    "url": "/FCC-pomodoro-clock/static/js/2.13a1966d.chunk.js"
+  },
+  {
+    "revision": "f5eeca88c65df516af9519a8bb5412f5",
+    "url": "/FCC-pomodoro-clock/static/js/2.13a1966d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cdc2a3ecebc3f577837",
+    "url": "/FCC-pomodoro-clock/static/js/main.d786e823.chunk.js"
+  },
+  {
+    "revision": "4e56ad8cf78e53fb7a94",
+    "url": "/FCC-pomodoro-clock/static/js/runtime-main.5a87d56a.js"
+  }
+]);
